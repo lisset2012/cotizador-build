16ebece0115c1744193745228c6ff51675c7cbf3
Lisset Torres <lisset2012@gmail.com>
2018-11-01 21:40:56 -0400

    first commi

---
+++ precache-manifest.63b52bdc658d85381a66ee9322442871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ff375213cccaba1cd45",
+    "url": "/static/js/main.4ff37521.chunk.js"
+  },
+  {
+    "revision": "4adba22359e7c069e0d8",
+    "url": "/static/js/1.4adba223.chunk.js"
+  },
+  {
+    "revision": "4ff375213cccaba1cd45",
+    "url": "/static/css/main.5fcb1b09.chunk.css"
+  },
+  {
+    "revision": "93c1aad4338f11e45517798e62158e0a",
+    "url": "/index.html"
+  }
+];
